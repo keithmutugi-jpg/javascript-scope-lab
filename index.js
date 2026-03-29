e217ccf7b3ba8558ce2f606b0c3b5a6ee48be0ed
Keith Mutugi <keithmutugi64@gmail.com>
2026-03-29 16:33:10 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Write your solution in this file!-
+// Global scope variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function to add burgers
+function addBurger() {
+  // Function-scoped variable
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+
+  // Block scope
+  if (true) {
+    const anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+  }
+}
+
+// Function to change featured drink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
